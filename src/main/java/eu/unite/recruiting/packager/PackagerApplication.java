9298e4bc865a08c335905e4a1bf8c9596ccfb9ad
it.recruiting.automation <it.recruiting.automation@unite.eu>
2022-06-27 10:49:21 +0000

    Add description of coding challenge :rocket:

---
+++ src/main/java/eu/unite/recruiting/packager/PackagerApplication.java
@@ -0,0 +1,13 @@
+package eu.unite.recruiting.packager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/eu/unite/recruiting/packager/rest/PackagerRestController.java
@@ -0,0 +1,15 @@
+package eu.unite.recruiting.packager.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class PackagerRestController {
+
+    @GetMapping("hello")
+    String helloWorld() {
+        return "Hello, world!";
+    }
+
+}
